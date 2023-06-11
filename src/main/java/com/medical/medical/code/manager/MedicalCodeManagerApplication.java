14c5b9d48e71a9433372d8111bc346cb13e8bf06
PAYAM SOUDACHI <payams@energizeglobal.com>
2023-06-11 19:30:18 +0400

    Add spring boot main application

---
+++ src/main/java/com/medical/medical/code/manager/MedicalCodeManagerApplication.java
@@ -0,0 +1,13 @@
+package com.medical.medical.code.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalCodeManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalCodeManagerApplication.class, args);
+	}
+
+}
